5fda837a0441623817ceeed43c4a8e9e47e93406
Hassan <hassan.nasir@acruxtek.com>
2023-01-17 14:32:48 +0500

    text changes

--- pages/index.js
+++ pages/index.js
@@ -23,19 +23,6 @@ export default function Home({ allPostsData }) {
           Having development background, with a year of experience in designing,
           developing and maintaining web projects using different tools and
           technologies. <br></br> <br></br>
-          {/* Expertise:<br></br><br></br><br></br>
-          - HTML, CSS, JavaScript, Java, ReactJS, AngularJS, NodeJS, <br></br><br></br>
-          - Web services (Restful APIs) <br></br><br></br>
-          - SQL Server2000/2005, MySQL Database Knowledge About: - MERN/MEAN Stack Core<br></br><br></br>
-          Competencies:<br></br><br></br><br></br>
-          - Professional reputation as intelligent, creative and hardworking.<br></br><br></br>
-          - Good interpersonal skills, communication skills and a
-          team player.<br></br><br></br>
-          - Ability to perform well under a fast paced environment
-          and multiple assignments.<br></br><br></br>
-          - Analysis and Reporting.<br></br><br></br>
-          - Problem Solving and Decision Making Love the web and developing for it as well as
-          breaking its conventions. */}
         </p>
         <p>
           (This is a sample website - you’ll be building a site like this on{" "}
